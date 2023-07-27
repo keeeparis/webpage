4288eb70f0b01ea7d4eb4e573e1fc9013de2eadd
Vladimir Trotsenko <vladimir.tr@musan.kz>
2023-07-27 18:43:56 +0600

    feat: change font load; add analytics; add sitemap; add robots.txt;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://seesaw.kz',
-  scopedStyleStrategy: 'class'
+  scopedStyleStrategy: 'class',
+  integrations: [sitemap()]
 });
---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+const config = {
+  GTM: import.meta.env.PUBLIC_GTM
+}
+
+export default config
